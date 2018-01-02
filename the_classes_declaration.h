554394d4039d59ef46da6ac0c9273a452b167356
yurunyang1998 <674860268@qq.com>
2018-01-02 17:56:41 +0800

    build linkform successful

--- the_classes_declaration.h
+++ the_classes_declaration.h
@@ -1,24 +1,24 @@
 //
 // Created by yurunyang on 18-1-1.
 //
-#include "string"
+#include <string>
 
 #ifndef THE_GPS_THE_CLASSES_DECLARATION_H
 #define THE_GPS_THE_CLASSES_DECLARATION_H
 
 #include <vector>
 //the link of the map
-
+using namespace std;
 class Arcnode
 {
 public:
     Arcnode(int adjvexx=0,int weightt=0,std::string namee= "");
     ~Arcnode();
-    void add_linknode(int num); // num表示这个节点邻接了几个点
+    void add_linknode(int adjvex,int adjweight,std::string name); // num表示这个节点邻接了几个点
     void print_linknode();   //输出当前节点所邻接的节点
 
 
-private:
+public:
     int adjvex;//下标
     int weight;//上一个节点到该点的路径长度
     std::string name; //实际的名字，如图书馆
@@ -32,11 +32,14 @@ private:
 class Allmap
 {
 public:
-    Allmap(int num);
+    Allmap();
     ~Allmap();
+    void readline(string mapfile); //读取mapfile到vector  of line 里面
+    void addArcnode(); //建立Arcnodes
 
-private:
+public:
     std::vector<Arcnode> map;
+    std::vector<std::string> vector_of_line;
     int vexnum; //节点数
     int arcnum; //弧的数目
 
--- the_realize_of_classes.cpp
+++ the_realize_of_classes.cpp
@@ -3,6 +3,66 @@
 //
 #include "iostream"
 #include "the_classes_declaration.h"
+#include <string>
+#include <vector>
+#include <fstream>
+
+using  namespace std;
+
+//下面这个是split函数
+/////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////
+void SplitString(const string& s, vector<string>& v, const string& c)
+{
+    string::size_type pos1, pos2;
+    pos2 = s.find(c);
+    pos1 = 0;
+    while(string::npos != pos2)
+    {
+        v.push_back(s.substr(pos1, pos2-pos1));
+
+        pos1 = pos2 + c.size();
+        pos2 = s.find(c, pos1);
+    }
+    if(pos1 != s.length())
+        v.push_back(s.substr(pos1));
+}
+
+////////////////////////////////////////////////////////////
+////////////////////////////////////////////////////////////
+
+
+
+//下面这个是按行读取文件的函数
+///////////////////////////////////////////////////////////
+///////////////////////////////////////////////////////////
+int  Readlines(string mapfile,vector<string> *vector_of_line)
+{
+    ifstream in(mapfile);
+    string filename;
+    string line;
+
+    if(in) // 有该文件
+    {
+        vector_of_line->clear();
+        while (getline(in, line)) // line中不包括每行的换行符
+        {
+            vector_of_line->push_back(line);
+        }
+        in.close();
+    }
+    else // 没有该文件
+    {
+        cout <<"no such file" << endl;
+    }
+
+    return 0;
+}
+///////////////////////////////////////////////////
+///////////////////////////////////////////////////
+
+
+
 Arcnode::Arcnode(int adjvexx, int weightt, std::string namee) {
     adjvex = adjvexx;
     weight = weightt;
@@ -14,31 +74,15 @@ Arcnode::~Arcnode(){}
 
 
 //为当前节点建立邻接顶点
-void Arcnode::add_linknode(int num) {
+void Arcnode::add_linknode(int adjvex,int adjweight,std::string name) {
 
     int i =0;
-    for(i=0;i<num;i++) {
-
-        int temp_adjvex=0;
-        int temp_adjweight = 0;
-        std::string  temp_name = "";
-
-        std::cout<<"请输入节点序号" <<std::endl;
-        std::cin>> temp_adjvex;
-
-        std::cout<< "请输入上一个节点到这个节点的长度"<<std::endl;
-        std::cin>> temp_adjweight;
-
-        std::cout<< "请输入这个节点的名字" <<std::endl;
-        std::cin>> temp_name;
-
-        Arcnode new_node(temp_adjvex,temp_adjweight,temp_name);
-        linknode.push_back(new_node);
-    }
+    Arcnode new_node(adjvex,adjweight,name);
+    linknode.push_back(new_node);
 
 };
 
-
+//////////这个是用来测试输出的
 void Arcnode::print_linknode(){
     std::cout<<adjvex<<","<<weight<<","<<name<<","<< std::endl;
     std::vector<Arcnode>::size_type  size_type =0;
@@ -47,14 +91,62 @@ void Arcnode::print_linknode(){
         std::cout<<linknode[size_type].name<<std::endl;
         size_type++;
     }
+}
+////////////////////////////////////
+
+//
+Allmap::Allmap(){
+
+};
+Allmap::~Allmap() {};
 
 
+///读取mapfile到vector  of line 里面
+void Allmap::readline(string mapfile) {
 
-
-
+    Readlines(mapfile,&vector_of_line);
 
 }
 
-//
-Allmap::Allmap(int num){};
-Allmap::~Allmap() {};
+void Allmap::addArcnode() {
+    if (vector_of_line.size() ==0)
+    {
+        std::cout<<"还未将mapfile读取vector"<<endl;
+        return;
+    }
+
+    int linesnum = vector_of_line.size();
+    int num = 15;
+    vector<string>::size_type iter;
+
+    for (iter=0;iter<num;iter++)
+    {
+        vector<string> templine , templine2;
+        SplitString(vector_of_line[iter],templine,"*");
+        //cout<<vector_of_line[iter]<<endl;  //*
+        SplitString(templine[0],templine2," ");
+
+        int temp = std::stoi(templine2[0]); //将string 转化为 int
+
+        //Arcnode * new_node = new Arcnode(temp,0,templine2[1]);   //*
+        Arcnode new_node(temp,0,templine2[1]);     //这个是头结点
+        vector<string>::size_type size ;
+
+        for(size=1;size<templine.size();size++)
+        {
+            vector<string> templine3 ;
+            SplitString(templine[size],templine3," ");
+            int tempnode = std::stoi(templine3[0]);
+            int tempweight = std::stoi(templine3[1]);
+            new_node.add_linknode(tempnode,tempweight,templine3[2]);
+
+        }
+
+        map.push_back(new_node);
+    }
+    std::cout <<2<<endl;
+    cout<<map[0].linknode[1].name<<endl;
+    cout <<3<<endl;
+
+
+}
